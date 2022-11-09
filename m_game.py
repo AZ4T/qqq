a9af5adf2ca7d67d8b8162397616577e09705a6c
Khan <tumarbatr@gmail.com>
2022-11-09 19:01:08 +0600

    added window rendering

--- m_game.py
+++ m_game.py
@@ -1,2 +1,24 @@
-https://pythonru.com/uroki/biblioteka-pygame-chast-1-vvedenie
-asd
+import pygame as p
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+p.init()
+p.mixer.init()
+screen = p.display.set_mode((WIDTH, HEIGHT))
+p.display.set_caption("MY_GAME")
+clock = p.time.Clock()
+
+running = True
+
+while running:
+    screen.fill(WHITE)
+
+    p.display.flip()
