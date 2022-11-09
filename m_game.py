ba3b9823d8cce96f40ee78253154c5deb3ac3dfa
AZ4T <bolatbekov718@gmail.com>
2022-11-09 15:51:42 +0600

    1

--- m_game.py
+++ m_game.py
@@ -1,10 +1,1 @@
-n = int(input())
-fst = 0
-
-a = list(map(int(input().split())))
-
-for i in a:
-    fst = max(fst, i)
-for j in a:
-    if j < fst and j != fst:
-        print(j)
+https://pythonru.com/uroki/biblioteka-pygame-chast-1-vvedenie
